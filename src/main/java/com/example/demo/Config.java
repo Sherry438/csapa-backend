245460c4a6fb1ae9fd8447f488699cb823b6e274
Sherry438 <shimo071024@gmail.com>
2025-04-02 16:38:45 +0800

    test for pushing

--- src/main/java/com/example/demo/Config.java
+++ src/main/java/com/example/demo/Config.java
@@ -1,5 +1,5 @@
 package com.example.demo;
-
+// test for github push
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
